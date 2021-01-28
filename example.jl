17441dffb73f5363075c8b0e437874bdd0b78f1c
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-01-28 11:36:46 -0500

    changing plot names, increasing barycentric model resolution, added using_GPs keyword which I'll likely remove now lol

--- example.jl
+++ example.jl
@@ -53,7 +53,7 @@ function build_gp_base(θ)
 end
 
 build_gp(θ) = to_sde(build_gp_base(θ), SArrayStorage(Float64))
-# build_gp(θ) = to_sde(build_gp_base(θ))
+build_gp_slow(θ) = to_sde(build_gp_base(θ))
 
 plot_spectrum(; kwargs...) = plot(; xlabel = "Wavelength (nm)", ylabel = "Continuum Normalized Flux", dpi = 400, kwargs...)
 plot_rv(; kwargs...) = plot(; xlabel = "Time (d)", ylabel = "RV (m/s)", dpi = 400, kwargs...)
@@ -61,17 +61,23 @@ plot_rv(; kwargs...) = plot(; xlabel = "Time (d)", ylabel = "RV (m/s)", dpi = 40
 function get_marginal_GP(
     finite_GP::Distribution{Multivariate,Continuous},
     ys::AbstractVector,
-    xs::AbstractVector)
-    gp_post = posterior(finite_GP, ys)
+    xs::AbstractVector;
+    og_gp=false)
+    if typeof(og_gp)==Bool
+        gp_post = posterior(finite_GP, ys)
+    else
+        gp_post = og_gp | (finite_GP ← ys)
+    end
     gpx_post = gp_post(xs)
     return TemporalGPs.marginals(gpx_post)
 end
 
+
 function get_mean_GP(
     finite_GP::Distribution{Multivariate,Continuous},
     ys::AbstractVector,
-    xs::AbstractVector)
-    return mean.(get_marginal_GP(finite_GP, ys, xs))
+    xs::AbstractVector; kwargs...)
+    return mean.(get_marginal_GP(finite_GP, ys, xs; kwargs...))
 end
 
 struct SpectraHolder{T<:AbstractArray{<:Real,1}}
@@ -101,6 +107,8 @@ end
 @load "E:/telfitting/telfitting_workspace.jld2"
 
 ## 1) Estimating stellar template
+star_template_res = 2 * sqrt(2) * obs_resolution
+
 max_bary_z = 2.5 * bary_K / light_speed
 min_temp_wav = (1 - max_bary_z) * min_wav
 max_temp_wav = (1 + max_bary_z) * max_wav
@@ -151,22 +159,22 @@ predict_plot = plot_spectrum(; legend = :bottomright, size=(1200, 800))
 plot!(predict_plot, λ_star_template, μ_star, st=:line, lw=1, label="Predicted Quiet")
 plot!(predict_plot, λ_nu[inds], quiet[inds], st=:line, lw=1, label="SOAP Quiet", alpha = 0.5)
 plot!(predict_plot, obs_λ, true_tels_mean, st=:line, lw=2, label="average telluric", alpha = 0.5)
-png(predict_plot, "figs/test3.png")
+png(predict_plot, "figs/spectrum.png")
 
 # Compare first guess at RVs to true signal
 plot_times = linspace(times_nu[1], times_nu[end], 1000)
 plot_rvs_kep = ustrip.(planet_ks.(plot_times .* 1u"d"))
 predict_plot = plot_rv()
-plot!(predict_plot, plot_times .% planet_P_nu, plot_rvs_kep, st=:line, color=:red, lw=1, label="Truth")
-plot!(predict_plot, times_nu .% planet_P_nu, rvs_naive, st=:scatter, ms=3, color=:blue, label="First Guess")
-png(predict_plot, "figs/test4.png")
+plot!(predict_plot, plot_times .% planet_P_nu, plot_rvs_kep, st=:line, color=:red, lw=1, label="Injected Keplerian")
+plot!(predict_plot, times_nu .% planet_P_nu, rvs_naive, st=:scatter, ms=3, color=:blue, label="Before model")
+png(predict_plot, "figs/model_0_phase.png")
 
 # Compare RV differences to actual RVs from activity
 rvs_kep_nu = ustrip.(rvs_kep)
 predict_plot = plot_rv()
-plot!(predict_plot, times_nu, rvs_naive - rvs_kep_nu, st=:scatter, ms=3, color=:blue, label="First Guess - Truth")
-plot!(predict_plot, times_nu, rvs_activ_noisy, st=:scatter, ms=3, color=:red, label="Actvity (with obs. SNR and resolution)")
-png(predict_plot, "figs/test5.png")
+plot!(predict_plot, times_nu, rvs_activ_noisy, st=:scatter, ms=3, color=:red, label="Activity (with obs. SNR and resolution)")
+plot!(predict_plot, times_nu, rvs_naive - rvs_kep_nu, st=:scatter, ms=3, color=:blue, label="Before model")
+png(predict_plot, "figs/model_0.png")
 
 ## 2) Use estimated stellar template to estimate tellurics
 
@@ -195,11 +203,11 @@ predict_plot = plot_spectrum(; legend = :bottomright, size=(1200, 800))
 # predict_plot = plot_spectrum(; xlim=(627.8,628.3), legend=:bottomright)
 # zoom in on h2o lines
 # predict_plot = plot_spectrum(; xlim=(651.5,652), legend=:bottomright)
-plot!(predict_plot, obs_λ, true_tels_mean / norm(true_tels_mean), st=:line, lw=1.5, label="Average Telluric")
-plot!(predict_plot, obs_λ, μ_tel / norm(μ_tel), st=:line, lw=1, label="Telluric guess mean", alpha=0.8)
+plot!(predict_plot, obs_λ, true_tels_mean / norm(true_tels_mean), st=:line, lw=1.5, label="Average telluric")
+plot!(predict_plot, obs_λ, μ_tel / norm(μ_tel), st=:line, lw=1, label="Intial telluric model mean", alpha=0.8)
 # plot!(predict_plot, obs_λ, M_tel[:,1], st=:line, lw=1, label="Telluric guess component 1", alpha=0.5)
 # plot!(predict_plot, obs_λ, M_tel[:,2], st=:line, lw=1, label="Telluric guess component 2", alpha=0.5)
-png(predict_plot, "figs/test6.png")
+png(predict_plot, "figs/mean_tel.png")
 
 ## 3) Use estimated tellurics template to make first pass at improving RVs
 
@@ -211,16 +219,16 @@ _, M_star, s_star, _, rvs_notel = DPCA(flux_bary, λ_star_template; template = �
 
 # Compare second guess at RVs to true signal
 predict_plot = plot_rv()
-plot!(predict_plot, plot_times .% planet_P_nu, plot_rvs_kep, st=:line, color=:red, lw=1, label="Truth")
-plot!(predict_plot, times_nu .% planet_P_nu, rvs_naive, st=:scatter, ms=3, color=:blue, label="First Guess")
-plot!(predict_plot, times_nu .% planet_P_nu, rvs_notel, st=:scatter, ms=3, color=:green, label="Second Guess")
-png(predict_plot, "figs/test7.png")
+plot!(predict_plot, plot_times .% planet_P_nu, plot_rvs_kep, st=:line, color=:red, lw=1, label="Injected Keplerian")
+plot!(predict_plot, times_nu .% planet_P_nu, rvs_naive, st=:scatter, ms=3, color=:blue, label="Before model")
+plot!(predict_plot, times_nu .% planet_P_nu, rvs_notel, st=:scatter, ms=3, color=:lightgreen, label="Before optimization")
+png(predict_plot, "figs/model_1_phase.png")
 
 # Compare RV differences to actual RVs from activity
 predict_plot = plot_rv()
-plot!(predict_plot, times_nu, rvs_activ_noisy, st=:scatter, ms=3, color=:red, label="Actvity (with obs. SNR and resolution)")
-plot!(predict_plot, times_nu, rvs_notel - rvs_kep_nu, st=:scatter, ms=3, color=:green, label="Second Guess - Truth")
-png(predict_plot, "figs/test8.png")
+plot!(predict_plot, times_nu, rvs_activ_noisy, st=:scatter, ms=3, color=:red, label="Activity (with obs. SNR and resolution)")
+plot!(predict_plot, times_nu, rvs_notel - rvs_kep_nu, st=:scatter, ms=3, color=:lightgreen, label="Before optimization")
+png(predict_plot, "figs/model_1.png")
 
 std((rvs_naive - rvs_kep_nu) - rvs_activ_no_noise)
 std((rvs_notel - rvs_kep_nu) - rvs_activ_no_noise)
@@ -269,12 +277,20 @@ function status_plot(θ_tot; plot_epoch=1, kwargs...)
     # predict_plot = plot_spectrum(; xlim=(651.5,652), legend=:bottomleft, size=(800,1200), layout = l)  # h2o
     predict_plot = plot_spectrum(; xlim = (647, 656), legend = :bottomleft, size=(800,1200), layout = l)  # h2o
     plot!(predict_plot[1], obs_λ, true_tels[:, plot_epoch], label="true tel")
-    plot!(predict_plot[1], obs_λ, flux_obs[:, plot_epoch] ./ (star_model_result[1] + rv_model_result[1]), label="predicted tel", alpha = 0.5)
+    if !using_GPs
+        plot!(predict_plot[1], obs_λ, flux_obs[:, plot_epoch] ./ (star_model_result[:, plot_epoch] + rv_model_result[:, plot_epoch]), label="predicted tel", alpha = 0.5)
+    else
+        plot!(predict_plot[1], obs_λ, flux_obs[:, plot_epoch] ./ (star_model_result[1] + rv_model_result[1]), label="predicted tel", alpha = 0.5)
+    end
     plot!(predict_plot[1], obs_λ, tel_model_result[:, plot_epoch], label="model tel: $tracker", alpha = 0.5)
     plot_bary_λs = exp.(Spectra[plot_epoch].log_λ_bary)
     plot!(predict_plot[2], plot_bary_λs, flux_obs[:, plot_epoch] ./ true_tels[:, plot_epoch], label="true star", )
     plot!(predict_plot[2], plot_bary_λs, flux_obs[:, plot_epoch] ./ tel_model_result[:, plot_epoch], label="predicted star", alpha = 0.5)
-    plot!(predict_plot[2], plot_bary_λs, star_model_result[1] + rv_model_result[1], label="model star: $tracker", alpha = 0.5)
+    if !using_GPs
+        plot!(predict_plot[2], plot_bary_λs, star_model_result[:, plot_epoch] + rv_model_result[:, plot_epoch], label="model star: $tracker", alpha = 0.5)
+    else
+        plot!(predict_plot[2], plot_bary_λs, star_model_result[1] + rv_model_result[1], label="model star: $tracker", alpha = 0.5)
+    end
     display(predict_plot)
 end
 
@@ -319,9 +335,10 @@ function model_prior(θ, coeffs::Vector{<:Real})
     L1(θ[2])
 end
 
+tel_model_result = ones(size(telluric_obs))
 tel_model(θ) = linear_model(θ)
-tel_prior(θ) = model_prior(θ, [2e2, 1e2, 1e3, 1e3, 1e6])
-# tel_prior(θ) = model_prior(θ, 0. * [2e2, 1e2, 1e3, 1e3, 1e6])
+prior_factor = 10
+tel_prior(θ) = model_prior(θ, prior_factor .* [2e2, 1e2, 1e3, 1e3, 1e6])
 
 lower_inds = zeros(Int, size(telluric_obs))
 ratios = zeros(size(telluric_obs))
@@ -335,44 +352,87 @@ for j in 1:size(telluric_obs, 2)
     end
     lower_inds[:, j] .+= (j - 1) * len_bary
 end
-gpx_template = SOAP_gp(log_λ_star_template, SOAP_gp_ridge)
-lower_inds
 
+# SOAP_gp_slow = build_gp_slow(θ1)
+# SOAP_gp_slow_bary_temp = SOAP_gp_slow(log_λ_star_template)
 
-function spectra_interp(bary_vals; epochs=1:n_obs, method="linear")
-    @assert method in ["linear", "alt linear", "GP", "sinc"]
-    if method == "linear"
-        model_result = [(bary_vals[lower_inds[:, i]] .* (1 .- ratios[:, i])) + (bary_vals[lower_inds[:, i] .+ 1] .* (ratios[:, i])) for i in epochs]
-    elseif method == "alt linear"
-        model_result = [LinearInterpolation(log_λ_star_template,
-            view(bary_vals, :, i)).(Spectra[i].log_λ_bary) for i in epochs]
-    elseif method == "GP"
-        model_result = [get_mean_GP(
-            gpx_template,
-            view(bary_vals, :, i) .- 1,
-            Spectra[i].log_λ_bary) .+ 1 for i in epochs]
-    elseif method == "sinc"
-        model_result = [spectra_interpolate(log_λ_star_template,
-            Spectra[i].log_λ_bary,
-            view(bary_vals, :, i)) for i in epochs]
+gpx_template = build_gp(θ1)(log_λ_star_template, SOAP_gp_ridge)
+# gpx_template = build_gp_slow(θ1)(log_λ_star_template, SOAP_gp_ridge)
+# SOAP_gp_stheno = build_gp_base(θ1)
+# gpx_template = SOAP_gp_stheno(log_λ_star_template, 1e10*SOAP_gp_ridge)
+using_GPs = false
+if !using_GPs
+    function spectra_interp(bary_vals; epochs=1:n_obs, method="linear")
+        @assert method in ["linear", "alt linear", "GP", "sinc"]
+        if method == "linear"
+            model_result = (bary_vals[lower_inds] .* (1 .- ratios)) + (bary_vals[lower_inds .+ 1] .* (ratios))
+        else
+            model_result = zeros(size(telluric_obs))
+            if method == "alt linear"
+                for i in epochs
+                    model_result[:, i] = LinearInterpolation(log_λ_star_template,
+                        view(bary_vals, :, i)).(Spectra[i].log_λ_bary)
+                end
+            elseif method == "GP"
+                for i in epochs
+                    model_result[:, i] = get_mean_GP(
+                        gpx_template,
+                        view(bary_vals, :, i) .- 1,
+                        Spectra[i].log_λ_bary) .+ 1
+                end
+            elseif method == "sinc"
+                for i in epochs
+                    model_result[:, i] = spectra_interpolate(log_λ_star_template,
+                        Spectra[i].log_λ_bary,
+                        view(bary_vals, :, i))
+                end
+            end
+        end
+        return model_result
     end
-    return model_result
+else
+    function spectra_interp(bary_vals; epochs=1:n_obs, method="GP")
+        @assert method in ["linear", "alt linear", "GP", "sinc"]
+        if method == "linear"
+            model_result = [(bary_vals[lower_inds[:, i]] .* (1 .- ratios[:, i])) + (bary_vals[lower_inds[:, i] .+ 1] .* (ratios[:, i])) for i in epochs]
+        elseif method == "alt linear"
+            model_result = [LinearInterpolation(log_λ_star_template,
+                view(bary_vals, :, i)).(Spectra[i].log_λ_bary) for i in epochs]
+        elseif method == "GP"
+            model_result = [get_mean_GP(
+                gpx_template,
+                view(bary_vals, :, i) .- 1,
+                Spectra[i].log_λ_bary) .+ 1 for i in epochs]
+        elseif method == "sinc"
+            model_result = [spectra_interpolate(log_λ_star_template,
+                Spectra[i].log_λ_bary,
+                view(bary_vals, :, i)) for i in epochs]
+        end
+        return model_result
+    end
 end
 
+star_model_result = ones(size(telluric_obs))
 star_model(θ; kwargs...) = spectra_interp(linear_model(θ); kwargs...)
-star_prior(θ) = model_prior(θ, [2e-2, 1e-2, 1e2, 1e5, 1e6])
-# star_prior(θ) = model_prior(θ, 0 .* [2e-2, 1e-2, 1e2, 1e5, 1e6])
+# star_prior(θ) = model_prior(θ, [2e-2, 1e-2, 1e2, 1e5, 1e6])
+star_prior(θ) = model_prior(θ, prior_factor .* [2e-2, 1e-2, 1e2, 1e5, 1e6])
 
+rv_model_result = ones(size(telluric_obs))
 rv_model(μ_star, θ; kwargs...) = _rv_model(calc_doppler_component_RVSKL(λ_star_template, μ_star), θ; kwargs...)
 _rv_model(M_rv, θ; kwargs...) = spectra_interp(M_rv * θ[1]; kwargs...)
 M_rv, s_rv = M_star[:, 1], s_star[1, :]'
 
-function _loss(tel_model_result, star_model_result, rv_model_result)
-    ans = 0
-    for i in length(star_model_result)
-        ans += sum((((tel_model_result[:, i] .* (star_model_result[i] + rv_model_result[i])) - flux_obs[:, i]) .^ 2) ./ obs_var)
+if !using_GPs
+    _loss(tel_model_result, star_model_result, rv_model_result) =
+        sum((((tel_model_result .* (star_model_result + rv_model_result)) - flux_obs) .^ 2) ./ obs_var)
+else
+    function _loss(tel_model_result, star_model_result, rv_model_result)
+        ans = 0
+        for i in length(star_model_result)
+            ans += sum((((tel_model_result[:, i] .* (star_model_result[i] + rv_model_result[i])) - flux_obs[:, i]) .^ 2) ./ obs_var)
+        end
+        return ans
     end
-    return ans
 end
 
 function loss(θ;
@@ -387,12 +447,9 @@ loss_star(θ) = _loss(tel_model_result, star_model(θ), rv_model_result) +
     star_prior(θ)
 loss_rv(θ) = _loss(tel_model_result, star_model_result, _rv_model(M_rv, θ))
 
-function θ_holder!(θ_holder, θ, inds; recalc_M_rv::Bool=true)
+function θ_holder!(θ_holder, θ, inds)
     for i in 1:length(inds)
         θ_holder[i][:,:] = reshape(θ[inds[i]], size(θ_holder[i]))
-    end
-    if recalc_M_rv
-        M_rv[:, :] = calc_doppler_component_RVSKL(λ_star_template, μ_star)
     end
 end
 function θ_holder_to_θ(θ_holder, inds)
@@ -426,8 +483,8 @@ inds_tel = relevant_inds(θ_tel)
 inds_star = relevant_inds(θ_star)
 inds_rv = relevant_inds(θ_rv)
 
-function f(θ, θ_holder, inds, loss_func)
-    θ_holder!(θ_holder, θ, inds)
+function f(θ, θ_holder, inds, loss_func; kwargs...)
+    θ_holder!(θ_holder, θ, inds; kwargs...)
     return loss_func(θ_holder)
 end
 f_tel(θ) = f(θ, θ_tel, inds_tel, loss_tel)
@@ -449,47 +506,43 @@ resid_stds = [std((rvs_notel - rvs_kep_nu) - rvs_activ_no_noise)]
 losses = [loss(θ_tot)]
 tracker = 0
 
-# plot(M_star_var)
-# plot(s_star_var')
-# plot(μ_star)
-#
-# plot(M_tel)
-# plot(s_tel')
-# plot(μ_tel)
-#
-# plot(M_rv)
-# plot(s_rv')
-#
-# plot(star_model(θ_star)[:, 13])
+# status_plot(θ_tot; plot_epoch=plot_epoch, method="GP")
+# status_plot(θ_tot; plot_epoch=plot_epoch, method="linear")
+# status_plot(θ_tot; plot_epoch=plot_epoch, method="alt linear")
+status_plot(θ_tot; plot_epoch=plot_epoch)
 
-star_model(θ_star; method="linear")
-
-status_plot(θ_tot; plot_epoch=10)
-status_plot(θ_tot; plot_epoch=10, method="alt linear")
-status_plot(θ_tot; plot_epoch=10, method="GP")
-status_plot(θ_tot; plot_epoch=10, method="linear")
-# status_plot(θ_tot; plot_epoch=10, method="linear")
 OOptions = Optim.Options(iterations=10, f_tol=1e-3, g_tol=1e5)
 
-optimize(f_tel, g_tel!, θ_holder_to_θ(θ_tel, inds_tel), LBFGS(), OOptions)
-optimize(f_rv, g_rv!, θ_holder_to_θ(θ_rv, inds_rv), LBFGS(), OOptions)
-optimize(f_star, g_star!, θ_holder_to_θ(θ_star, inds_star), LBFGS(), OOptions)
+tel_model_result[:, :] = tel_model(θ_tel)
+rv_model_result = rv_model(μ_star, θ_rv)
+star_model_result = star_model(θ_star)
+
+# optimize(f_tel, g_tel!, θ_holder_to_θ(θ_tel, inds_tel), LBFGS(), OOptions)
+# optimize(f_rv, g_rv!, θ_holder_to_θ(θ_rv, inds_rv), LBFGS(), OOptions)
+# optimize(f_star, g_star!, θ_holder_to_θ(θ_star, inds_star), LBFGS(), OOptions)
 
 println("guess $tracker, std=$(round(std(rvs_notel - rvs_kep_nu - rvs_activ_no_noise), digits=5))")
-for i in 1:4
+rvs_notel_opt = copy(rvs_notel)
+for i in 1:9
     tracker += 1
     println("guess $tracker")
 
-    optimize(f_star, g_star!, θ_holder_to_θ(θ_star, inds_star), LBFGS(), OOptions)
     optimize(f_tel, g_tel!, θ_holder_to_θ(θ_tel, inds_tel), LBFGS(), OOptions)
-    optimize(f_rv, g_rv!, θ_holder_to_θ(θ_rv, inds_rv), LBFGS(), OOptions)
+    tel_model_result[:, :] = tel_model(θ_tel)
 
-    rvs_notel[:] = (s_rv .* light_speed_nu)'
-    append!(resid_stds, [std(rvs_notel - rvs_kep_nu - rvs_activ_no_noise)])
+    optimize(f_rv, g_rv!, θ_holder_to_θ(θ_rv, inds_rv), LBFGS(), OOptions)
+    rv_model_result[:, :] = rv_model(μ_star, θ_rv)
+
+    optimize(f_star, g_star!, θ_holder_to_θ(θ_star, inds_star), LBFGS(), OOptions)
+    star_model_result[:, :] = star_model(θ_star)
+    M_rv[:, :] = calc_doppler_component_RVSKL(λ_star_template, μ_star)
+
+    rvs_notel_opt[:] = (s_rv .* light_speed_nu)'
+    append!(resid_stds, [std(rvs_notel_opt - rvs_kep_nu - rvs_activ_no_noise)])
     append!(losses, [loss(θ_tot)])
 
     println("loss   = $(loss(θ_tot))")
-    println("rv std = $(round(std((rvs_notel - rvs_kep_nu) - rvs_activ_no_noise), digits=5))")
+    println("rv std = $(round(std((rvs_notel_opt - rvs_kep_nu) - rvs_activ_no_noise), digits=5))")
     status_plot(θ_tot)
 end
 plot(resid_stds; xlabel="iter", ylabel="predicted RV - active RV RMS", legend=false)
@@ -499,13 +552,15 @@ plot(losses; xlabel="iter", ylabel="loss", legend=false)
 
 # Compare RV differences to actual RVs from activity
 predict_plot = plot_rv()
-plot!(predict_plot, times_nu, rvs_activ_noisy .- mean(rvs_activ_noisy), st=:scatter, ms=3, color=:red, label="Actvity (with obs. SNR and resolution)")
-plot!(predict_plot, times_nu, rvs_notel - rvs_kep_nu .- mean(rvs_notel - rvs_kep_nu), st=:scatter, ms=3, color=:green, label="Third Guess - Truth")
-png(predict_plot, "figs/test9.png")
+plot!(predict_plot, times_nu, rvs_activ_noisy, st=:scatter, ms=3, color=:red, label="Activity (with obs. SNR and resolution)")
+plot!(predict_plot, times_nu, rvs_notel - rvs_kep_nu, st=:scatter, ms=3, color=:lightgreen, label="Before optimization")
+plot!(predict_plot, times_nu, rvs_notel_opt - rvs_kep_nu, st=:scatter, ms=3, color=:darkgreen, label="After optimization")
+png(predict_plot, "figs/model_2.png")
 
 # Compare second guess at RVs to true signal
 predict_plot = plot_rv()
-plot!(predict_plot, plot_times .% planet_P_nu, plot_rvs_kep, st=:line, color=:red, lw=1, label="Truth")
-plot!(predict_plot, times_nu .% planet_P_nu, rvs_naive, st=:scatter, ms=3, color=:blue, label="First Guess")
-plot!(predict_plot, times_nu .% planet_P_nu, rvs_notel, st=:scatter, ms=3, color=:green, label="Third Guess")
-png(predict_plot, "figs/test10.png")
+plot!(predict_plot, plot_times .% planet_P_nu, plot_rvs_kep, st=:line, color=:red, lw=1, label="Injected Keplerian")
+plot!(predict_plot, times_nu .% planet_P_nu, rvs_naive, st=:scatter, ms=3, color=:blue, label="Before model")
+plot!(predict_plot, times_nu .% planet_P_nu, rvs_notel, st=:scatter, ms=3, color=:lightgreen, label="Before optimization")
+plot!(predict_plot, times_nu .% planet_P_nu, rvs_notel_opt, st=:scatter, ms=3, color=:darkgreen, label="After optimization")
+png(predict_plot, "figs/model_2_phase.png")
