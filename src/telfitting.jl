3e7f832f2648c36a009a2483b74262307030418b
christiangil <christianjgilbertson@gmail.com>
2021-03-10 10:36:03 -0800

    reordering script imports

--- src/telfitting.jl
+++ src/telfitting.jl
@@ -1,8 +1,8 @@
 module telfitting
 
 include("general_functions.jl")
-include("FluxOptTools.jl")
 include("DPCA_functions.jl")
 include("model_functions.jl")
+include("FluxOptTools.jl")
 
 end # module
