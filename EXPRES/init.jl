bf5e4ccf22d05ef08424ed80b87e6601a06f62b9
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-06-22 15:23:34 -0400

    removing redundant jld2 file save

--- EXPRES/init.jl
+++ EXPRES/init.jl
@@ -18,58 +18,47 @@ fits_target_str = star
 paths_to_search_for_param = ["EXPRES"]
 include("data_locs.jl")  # defines expres_data_path and expres_save_path
 
-init_jld2 = expres_save_path * target_subdir * "init.jld2"
-if !isfile(init_jld2)
-	# NOTE: make_manifest does not update its paths_to_search when default_paths_to_search is defined here, so if you change the line above, you must also include "paths_to_search=default_paths_to_search" in the make_manifest() function call below
-	pipeline_plan = PipelinePlan()
-	dont_make_plot!(pipeline_plan, :movie)
-	reset_all_needs!(pipeline_plan)
-	masks = Array{UnitRange, 2}(undef, sum(occursin.(r"\.fits$", readdir(expres_data_path * target_subdir))), 86)
+# NOTE: make_manifest does not update its paths_to_search when default_paths_to_search is defined here, so if you change the line above, you must also include "paths_to_search=default_paths_to_search" in the make_manifest() function call below
+pipeline_plan = PipelinePlan()
+dont_make_plot!(pipeline_plan, :movie)
+reset_all_needs!(pipeline_plan)
+masks = Array{UnitRange, 2}(undef, sum(occursin.(r"\.fits$", readdir(expres_data_path * target_subdir))), 86)
 
-	if need_to(pipeline_plan,:read_spectra)
-	    df_files = make_manifest(expres_data_path, target_subdir, EXPRES)
-	    # Reading in customized parameters from param.jl.
-	    eval(code_to_include_param_jl(paths_to_search=paths_to_search_for_param))
-	    # Reading in FITS files
-	    all_spectra = Spectra2DExtended[]
-	    for j in 1:size(masks, 1)
-	        row = eachrow(df_files_use)[j]
-	        spectra, mask = EXPRES.read_data(row; store_min_data=true, store_tellurics=true, normalization=:blaze, return_λ_obs=true, return_excalibur_mask=true)
-	        append!(all_spectra, [spectra])
-	        for i in 1:size(mask, 2)
-	            try
-	                x = findfirst(view(mask, :, i)):findlast(view(mask, :, i))
-	                masks[j, i] = x
-	                @assert all(mask[x, i] .== true)
-	            catch y
-	                if typeof(y)==MethodError
-	                    masks[j, i] = 0:0
-	                else
-	                    throw(y)
-	                end
-	            end
-	        end
-	    end
-	    GC.gc()
-	    dont_need_to!(pipeline_plan,:read_spectra)
-	end
-
-	inds = Vector{UnitRange}(undef, size(masks,2))
-	for i in 1:length(inds)
-	    inds[i] = maximum([mask[1] for mask in masks[:, i]]):minimum([mask[end] for mask in masks[:, i]])
-	end
-	# Something buggy if include order 1.  Order 86 essentially ignored due to tellurics. First and last orders have NaN issues
-	#max_orders = 12:83
-	# lfc_orders = 43:72
-	# order_list_timeseries = extract_orders(all_spectra, pipeline_plan; orders_to_use=lfc_orders, recalc=true)
-
-	times_nu = pipeline_plan.cache[:extract_orders].times
-	airmasses = [parse(Float64, md[:airmass]) for md in pipeline_plan.cache[:extract_orders].metadata]
-
-	@save init_jld2 times_nu airmasses inds all_spectra
-else
-	@load init_jld2 times_nu airmasses inds all_spectra
+if need_to(pipeline_plan,:read_spectra)
+    df_files = make_manifest(expres_data_path, target_subdir, EXPRES)
+    # Reading in customized parameters from param.jl.
+    eval(code_to_include_param_jl(paths_to_search=paths_to_search_for_param))
+    # Reading in FITS files
+    all_spectra = Spectra2DExtended[]
+    for j in 1:size(masks, 1)
+        row = eachrow(df_files_use)[j]
+        spectra, mask = EXPRES.read_data(row; store_min_data=true, store_tellurics=true, normalization=:blaze, return_λ_obs=true, return_excalibur_mask=true)
+        append!(all_spectra, [spectra])
+        for i in 1:size(mask, 2)
+            try
+                x = findfirst(view(mask, :, i)):findlast(view(mask, :, i))
+                masks[j, i] = x
+                @assert all(mask[x, i] .== true)
+            catch y
+                if typeof(y)==MethodError
+                    masks[j, i] = 0:0
+                else
+                    throw(y)
+                end
+            end
+        end
+    end
+    GC.gc()
+    dont_need_to!(pipeline_plan,:read_spectra)
 end
+
+inds = Vector{UnitRange}(undef, size(masks,2))
+for i in 1:length(inds)
+    inds[i] = maximum([mask[1] for mask in masks[:, i]]):minimum([mask[end] for mask in masks[:, i]])
+end
+
+times_nu = pipeline_plan.cache[:extract_orders].times
+airmasses = [parse(Float64, md[:airmass]) for md in pipeline_plan.cache[:extract_orders].metadata]
 
 ## Switching to my data format
 
@@ -95,3 +84,6 @@ for desired_order in findfirst(useful_orders):findlast(useful_orders)
 	SSOF.process!(tf_data; order=6)
 	@save expres_save_path * star * "/$(desired_order)_data.jld2" n_obs tf_data times_nu airmasses
 end
+
+# lfc_orders = 43:72
+# order_list_timeseries = extract_orders(all_spectra, pipeline_plan; orders_to_use=lfc_orders, recalc=true)
