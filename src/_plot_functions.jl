229bc7abffe5f085a5afb461a01be03a31ad5cfc
christiangil <christianjgilbertson@gmail.com>
2021-08-02 14:18:32 -0400

    looking into elbows of likelihood vs ncomponent plots

--- src/_plot_functions.jl
+++ src/_plot_functions.jl
@@ -117,3 +117,15 @@ function separate_status_plot(o::StellarSpectraObservationFitting.Output, d::Ste
     if display_plt; display(plt) end
     return plt
 end
+
+function component_test_plot(comp_ℓs::Matrix, test_n_comp_tel::AbstractVector, test_n_comp_star::AbstractVector; size=(_plt_size[1],_plt_size[2]*1.5))
+    plt = _my_plot(; ylabel="ℓ", layout=grid(2, 1), size=size)
+    for i in 1:length(test_n_comp_tel)
+        plot!(plt[1], test_n_comp_star, comp_ℓs[i, :]; label="$(test_n_comp_tel[i]) tel", xlabel="# of stellar components")
+    end
+    for i in 1:length(test_n_comp_star)
+        plot!(plt[2], test_n_comp_tel, comp_ℓs[:, i]; label="$(test_n_comp_star[i]) stellar", xlabel="# of telluric components")
+    end
+    display(plt)
+    return plt
+end
