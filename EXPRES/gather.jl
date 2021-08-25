ea329c27ee979b982b81dae4e8bcd9f677253b3a
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-08-25 10:05:08 -0400

    adding rored to gather.jl

--- EXPRES/gather.jl
+++ EXPRES/gather.jl
@@ -11,15 +11,16 @@ import StellarSpectraObservationFitting; SSOF = StellarSpectraObservationFitting
 stars = ["10700", "26965", "34411"]
 orders_list = [42:77, 40:77, 38:77]
 include("data_locs.jl")  # defines expres_data_path and expres_save_path
+prep_str = "noreg_"  # prep_str = ""
 
 function retrieve(order::Int, star::String)
-    @load expres_save_path*star*"/$(order)/results.jld2" model rv_errors
+    @load expres_save_path*star*"/$(order)/$(prep_str)results.jld2" model rv_errors
     rvs_notel_opt = SSOF.rvs(model)
     return rvs_notel_opt, rv_errors
 end
 
 function retrieve_md(order::Int, star::String)
-    @load expres_save_path*star*"/$(order)/model_decision.jld2" comp_ls ℓ aic bic ks test_n_comp_tel test_n_comp_star
+    @load expres_save_path*star*"/$(order)/$(prep_str)model_decision.jld2" comp_ls ℓ aic bic ks test_n_comp_tel test_n_comp_star
     ans_aic = argmin(aic)
     ans_bic = argmin(bic)
     n_comps = [test_n_comp_tel[ans_aic[1]], test_n_comp_star[ans_aic[2]]]
@@ -33,7 +34,7 @@ for star_ind in star_inds
     star = stars[star_ind]
     orders = orders_list[star_ind]
 
-    @load expres_save_path*star*"/$(orders[1])/data.jld2" n_obs times_nu airmasses
+    @load expres_save_path*star*"/$(orders[1])/$(prep_str)data.jld2" n_obs times_nu airmasses
     n_ord = length(orders)
     rvs = zeros(n_ord,  n_obs)
     rvs_σ = zeros(n_ord, n_obs)
@@ -51,6 +52,6 @@ for star_ind in star_inds
         end
     end
 
-    @save "$(star)_rvs.jld2" rvs rvs_σ n_obs times_nu airmasses n_ord
-    @save "$(star)_md.jld2" n_comps n_comps_bic robust
+    @save "$(prep_str)$(star)_rvs.jld2" rvs rvs_σ n_obs times_nu airmasses n_ord
+    @save "$(prep_str)$(star)_md.jld2" n_comps n_comps_bic robust
 end
