c0bf3d593de89919f4c41f09152a02adfb34a74b
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-09-07 12:29:21 -0400

    adding mask_functions.jl

--- src/StellarSpectraObservationFitting.jl
+++ src/StellarSpectraObservationFitting.jl
@@ -8,5 +8,6 @@ include("optimization_functions.jl")
 include("regularization_functions.jl")
 include("continuum_functions.jl")
 include("model_selection_functions.jl")
+include("mask_functions.jl")
 
 end # module
