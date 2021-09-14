f3331837ffcbeefc2080cc5a01dedbce78c442a9
christiangil <christianjgilbertson@gmail.com>
2021-09-14 16:02:56 -0400

    removing fluxopttools

--- src/StellarSpectraObservationFitting.jl
+++ src/StellarSpectraObservationFitting.jl
@@ -3,7 +3,6 @@ module StellarSpectraObservationFitting
 include("general_functions.jl")
 include("DPCA_functions.jl")
 include("model_functions.jl")
-include("FluxOptTools.jl")
 include("optimization_functions.jl")
 include("regularization_functions.jl")
 include("continuum_functions.jl")
