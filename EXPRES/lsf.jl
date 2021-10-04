7d9da4564a5e149e50ce2c0b135c8bb42d607e24
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-10-04 17:29:45 -0400

    creating lsf_broadeners and a safe version of  lsf_σ

--- EXPRES/lsf.jl
+++ EXPRES/lsf.jl
@@ -4,6 +4,7 @@ import StellarSpectraObservationFitting; SSOF = StellarSpectraObservationFitting
 
 ## Finding LSF width as a function of λ
 using Distributions
+using BandedMatrices
 using LinearAlgebra
 _recalc = true
 _inter_poly_order = 2
@@ -28,8 +29,8 @@ if _recalc
         unit_str = "Å?"
         xlab = "Wavelength ($unit_str)"
     end
-    λs = λs_func(eo."wavenumber [1/cm]")
-    σ = fwhm_2_σ(λs .* (eo."fwhm [1/cm]" ./ eo."wavenumber [1/cm]"))
+    _λs = λs_func(eo."wavenumber [1/cm]")
+    σ = SSOF.fwhm_2_σ.(_λs .* (eo."fwhm [1/cm]" ./ eo."wavenumber [1/cm]"))
 
     dm = ones(size(eo, 1), 1 + _inter_poly_order + _intra_poly_order)
     # orders = [i for i in 1:100 if i in eo.order]
@@ -52,48 +53,93 @@ if _recalc
     println("new lsf_σ w: ", _w)
     model = dm * _w
 else
-    _w = [-0.00047152836831767077, 0.00011337984500164679, -6.412301552294912e-6, -0.00020175981792570192, 0.015969041179354226]
+    _w = [-0.013170986773323784, 4.8136021892119126e-6, -3.772787891582741e-11, 2.3266186490906004e-5, 1.100967064582426e-7]
 end
 
-lsf_σ(logλ::Real, logλ_m_order_mean::Real) = [1, logλ, logλ*logλ, logλ_m_order_mean, logλ_m_order_mean*logλ_m_order_mean]' * _w
-function lsf_σ(logλ::AbstractVector, logλ_m_order_mean::AbstractVector)
-    dm = ones(length(logλ), 3 + _poly_order)
-    dm[:, 2] = logλ
-    dm[:, 3] = logλ .* logλ
-    dm[:, 4] = logλ_m_order_mean
-    for i in 2:_poly_order
-        dm[:, i+3] = logλ_m_order_mean .^ i
+lsf_σ_inter_order(wn::Real) = _w[1] + wn*(_w[2] + _w[3]*wn)
+lsf_σ_intra_order(wn_m_order_mean::Real) = wn_m_order_mean*(_w[4] + _w[5]*wn_m_order_mean)
+lsf_σ(wn::Real, wn_m_order_mean::Real) = lsf_σ_inter_order(wn) + lsf_σ_intra_order(wn_m_order_mean)
+_min_wn, _max_wn = extrema(_λs)
+function lsf_σ_safe(wn::Real, wn_m_order_mean::Real)
+    if _min_wn < wn < _max_wn
+        return lsf_σ(wn, wn_m_order_mean)
+    else
+        return lsf_σ_inter_order(wn)
+    end
+end
+function lsf_σ_inter_order(wn::AbstractVector)
+    dm = ones(length(wn), 3)
+    dm[:, 2] = wn
+    dm[:, 3] = wn .* wn
+    return dm * view(_w, 1:3)
+end
+
+function lsf_σ(wn::AbstractVector, wn_m_order_mean::AbstractVector)
+    dm = ones(length(wn), 1 + _inter_poly_order + _intra_poly_order)
+    dm[:, 2] = wn
+    for i in 2:_inter_poly_order
+        dm[:, i+1] = wn .^ i
+    end
+    dm[:, 2+_inter_poly_order] = wn_m_order_mean
+    for i in 2:_intra_poly_order
+        dm[:, i+_inter_poly_order+1] = wn_m_order_mean .^ i
     end
     return dm * _w
 end
+function lsf_σ_safe(wn::AbstractVector, wn_m_order_mean::AbstractVector)
+    if _min_wn < mean(wn) < _max_wn
+        return lsf_σ(wn, wn_m_order_mean)
+    else
+        return lsf_σ_inter_order(wn)
+    end
+end
 
-# plt = my_scatter(λs, σ; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
+# plt = my_scatter(_λs, σ; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
 # png(plt, "expres_lsf")
 #
 # inds1 = eo.order .== 38
 # inds2 = eo.order .== 39
-# plt = my_scatter(λs[inds1], σ[inds1]; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
-# scatter!(λs[inds2], σ[inds2]; label="")
+# plt = my_scatter(_λs[inds1], σ[inds1]; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
+# scatter!(_λs[inds2], σ[inds2]; label="")
 # png(plt, "expres_lsf_zoom")
 #
-plt = my_scatter(λs, σ; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
-for order in orders
-    inds_temp = eo.order .== order
-    plot!(λs[inds_temp], model[inds_temp]; label="", lw=4)
-end
-plt
-png(plt, "expres_lsf_model")
+# plt = my_scatter(_λs, σ; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
+# for order in orders
+#     inds_temp = eo.order .== order
+#     plot!(_λs[inds_temp], model[inds_temp]; label="", lw=4)
+# end
+# plt
+# png(plt, "expres_lsf_model")
 #
-# plt = my_scatter(λs[inds1], σ[inds1]; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
-# scatter!(λs[inds2], σ[inds2]; label="")
-# plot!(λs[inds1], model[inds1]; label="model", lw=6, c=plt_colors[6])
-# plot!(λs[inds2], model[inds2]; label="model", lw=6)
+# plt = my_scatter(_λs[inds1], σ[inds1]; label="", xlabel=xlab, ylabel="LSF σ ($unit_str)")
+# scatter!(_λs[inds2], σ[inds2]; label="")
+# plot!(_λs[inds1], model[inds1]; label="model", lw=6, c=plt_colors[6])
+# plot!(_λs[inds2], model[inds2]; label="model", lw=6)
 # png(plt, "expres_lsf_model_zoom")
 #
-resids = σ ./ model
-std(resids)
-# plt = my_scatter(λs, resids; label="data / model", xlabel=xlab, ylabel="LSF σ ($unit_str)")
+# resids = σ ./ model
+# std(resids)
+# plt = my_scatter(_λs, resids; label="data / model", xlabel=xlab, ylabel="LSF σ ($unit_str)")
 # png(plt, "expres_lsf_resids")
-# plt = scatter(λs[inds1], resids[inds1]; label="data / model", xlabel=xlab, ylabel="LSF σ ($unit_str)")
-# scatter!(λs[inds2], resids[inds2]; label="data / model")
+# plt = scatter(_λs[inds1], resids[inds1]; label="data / model", xlabel=xlab, ylabel="LSF σ ($unit_str)")
+# scatter!(_λs[inds2], resids[inds2]; label="data / model")
 # png(plt, "expres_lsf_resids_zoom")
+
+function lsf_broadener(λ::AbstractVector; safe::Bool=true)
+    if safe; @assert 1000 < mean(λ) < 50000  "Are you sure you're using λ (Å) and not wavenumber (1/cm) or log(λ)?" end
+    wn = SSOF.Å_to_wavenumber.(λ)
+    σs = lsf_σ_safe(wn, wn .- mean(wn))
+    nwn = -wn
+    holder = zeros(length(nwn), length(nwn))
+    maxb = 0
+    for i in 1:length(nwn)
+        lo, hi = SSOF.searchsortednearest(nwn, [nwn[i] - 3 * σs[i], nwn[i] + 3 * σs[i]])
+        lsf = Normal(wn[i], σs[i])
+        holder[i, lo:hi] = pdf.(lsf, wn[lo:hi])
+        holder[i, lo:hi] ./= sum(view(holder, i, lo:hi))
+        maxb = max(max(maxb, hi-i), i-lo)
+    end
+    return BandedMatrix(holder,(maxb,maxb))
+end
+lsf_broadener(λ::AbstractMatrix; kwargs...) =
+    [lsf_broadener(view(λ, :, i); kwargs...) for i in 1:size(λ, 2)]
