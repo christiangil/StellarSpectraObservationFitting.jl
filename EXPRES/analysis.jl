5491943994433c0f7ec8680e26d956247f708ecf
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-10-05 16:29:02 -0400

    still need to find source of memory leak

--- EXPRES/analysis.jl
+++ EXPRES/analysis.jl
@@ -34,7 +34,8 @@ if isfile(save_path*"results.jld2")
         @load save_path*"model_decision.jld2" comp_ls ℓ aic bic ks test_n_comp_tel test_n_comp_star
     end
 else
-    model_upscale = 2 * sqrt(2)
+    model_upscale = sqrt(2)
+    # model_upscale = 2 * sqrt(2)
     @time model = SSOF.OrderModel(data, "EXPRES", desired_order, star; n_comp_tel=8, n_comp_star=8, upscale=model_upscale)
     @time rvs_notel, rvs_naive, _, _ = SSOF.initialize!(model, data; use_gp=true)
     if !use_reg
