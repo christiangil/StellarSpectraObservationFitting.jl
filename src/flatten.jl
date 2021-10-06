effb6acddf358bb4e140085dff499b1b201e9886
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-10-06 14:46:02 -0400

    commenting out useless flatten

--- src/flatten.jl
+++ src/flatten.jl
@@ -2,14 +2,14 @@
 using ParameterHandling
 import ParameterHandling.flatten
 
-function flatten(::Type{T}, x::GenericData) where {T<:Real}
-    x_vec, unflatten = flatten(T, [getfield(x, i) for i in fieldnames(typeof(x))])
-    function unflatten_to_struct(v::Vector{T})
-        v_vec_vec = unflatten(v)
-        return GenericData(v_vec_vec...)
-    end
-    return x_vec, unflatten_to_struct
-end
+# function flatten(::Type{T}, x::GenericData) where {T<:Real}
+#     x_vec, unflatten = flatten(T, [getfield(x, i) for i in fieldnames(typeof(x))])
+#     function unflatten_to_struct(v::Vector{T})
+#         v_vec_vec = unflatten(v)
+#         return GenericData(v_vec_vec...)
+#     end
+#     return x_vec, unflatten_to_struct
+# end
 function flatten(::Type{T}, x::LinearModel) where {T<:Real}
     x_vec, unflatten = flatten(T, [getfield(x, i) for i in fieldnames(typeof(x))])
     function unflatten_to_struct(v::Vector{T})
