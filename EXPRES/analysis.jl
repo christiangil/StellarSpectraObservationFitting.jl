c04707781a3b0e684ab603a75d59e0cc1c167a52
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2021-10-18 19:06:38 -0400

    adding MKLSparse for SPEED

--- EXPRES/analysis.jl
+++ EXPRES/analysis.jl
@@ -6,6 +6,7 @@ using JLD2
 using Statistics
 import StellarSpectraObservationFitting; SSOF = StellarSpectraObservationFitting
 import StatsBase
+using MKLSparse
 
 ## Setting up necessary variables
 
