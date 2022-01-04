513e8f43d3ed1e36530b921b8f85a6974f83e9da
Chistian Gilbertson <chistianjgilbertson@gmail.com>
2022-01-04 11:18:14 -0500

    cleaning up prior_gp_functions.jl and adding SOAP_gp_ℓ_nabla()

--- src/prior_gp_functions.jl
+++ src/prior_gp_functions.jl
@@ -3,12 +3,11 @@
 using StaticArrays
 import TemporalGPs; TGP = TemporalGPs
 
-gp = SOAP_gp
-@assert typeof(gp.f.kernel.kernel.kernel) <: Matern52Kernel
+@assert typeof(SOAP_gp.f.kernel.kernel.kernel) <: Matern52Kernel
 
 ## Defining necessary params
 
-const σ²_kernel = gp.f.kernel.kernel.σ²[1]
+const σ²_kernel = SOAP_gp_params.var_kernel  # = SOAP_gp.f.kernel.kernel.σ²[1]
 
 # p = 2
 # ν = 2 * p + 1 / 2
@@ -31,14 +30,16 @@ const H_k = SMatrix{1,3}(H .* sqrt(σ²_kernel))  # this is how we deal with ker
 
 # Need one of these per timestep
 # They are constant if we have constant timestep
-function sde_prediction_matrices(Δx; σ²_meas::Real=1e-12, H_k::AbstractMatrix=H_k, P∞::AbstractMatrix=P∞, F::AbstractMatrix=F)
-    A_k = SMatrix{3,3}(exp(F * Δx))  # State transition matrix eq 6.23 in [2]?
+# σ²_meas and H_k only inlcuded as kwargs to prevent errors with passing kwargs...
+# in SOAP_gp_ℓ(y, Δx::Real; kwargs...)
+function SOAP_gp_sde_prediction_matrices(Δx; Δx_scaler::Real=SOAP_gp_params.λ, P∞::AbstractMatrix=P∞, F::AbstractMatrix=F, σ²_meas::Real=1e-12, H_k::AbstractMatrix=H_k)
+    A_k = SMatrix{3,3}(exp(F * Δx * Δx_scaler))  # State transition matrix eq 6.23 in [2]?
     Σ_k = SMatrix{3,3}(Symmetric(P∞) - A_k * Symmetric(P∞) * A_k')  # eq. 6.71 in [2], the process noise
     return A_k, Σ_k
 end
 
-function SOAP_gp_ℓ(y, Δx::Real; Δx_scaler::Real=only(gp.f.kernel.transform.s[1]), kwargs...)
-    A_k, Σ_k = sde_prediction_matrices(Δx * Δx_scaler; kwargs...)
+function SOAP_gp_ℓ(y, Δx::Real; kwargs...)
+    A_k, Σ_k = SOAP_gp_sde_prediction_matrices(Δx; kwargs...)
     return SOAP_gp_ℓ(y, A_k, Σ_k; kwargs...)
 end
 
@@ -46,12 +47,13 @@ end
 function SOAP_gp_ℓ(y, A_k::AbstractMatrix, Σ_k::AbstractMatrix; σ²_meas::Real=1e-12, H_k::AbstractMatrix=H_k, P∞::AbstractMatrix=P∞, F::AbstractMatrix=F)
 
     n = length(y)
+    n_state = 3
     ℓ = 0
-    m_k = MVector{3}(zeros(size(P∞, 1)))
+    m_k = @MVector zeros(n_state)
     P_k = MMatrix{3,3}(P∞)
-    m_kbar = @MVector zeros(3)
-    P_kbar = @MMatrix zeros(3,3)
-    K_k = @MMatrix zeros(3,1)
+    m_kbar = @MVector zeros(n_state)
+    P_kbar = @MMatrix zeros(n_state, n_state)
+    K_k = @MMatrix zeros(n_state, 1)
     for k in 1:n
         # prediction step
         m_kbar .= A_k * m_k  # state prediction
@@ -67,3 +69,44 @@ function SOAP_gp_ℓ(y, A_k::AbstractMatrix, Σ_k::AbstractMatrix; σ²_meas::Re
     end
     return (ℓ - n*log(2π))/2
 end
+
+
+function SOAP_gp_ℓ_nabla(y, Δx::Real; kwargs...)
+    A_k, Σ_k = SOAP_gp_sde_prediction_matrices(Δx; kwargs...)
+    return SOAP_gp_ℓ_nabla(y, A_k, Σ_k; kwargs...)
+end
+
+
+# removing things that Nabla doesn't like from SOAP_gp_ℓ
+function SOAP_gp_ℓ_nabla(y, A_k::AbstractMatrix, Σ_k::AbstractMatrix; σ²_meas::Real=1e-12, H_k::AbstractMatrix=H_k, P∞::AbstractMatrix=P∞, F::AbstractMatrix=F)
+
+    n = length(y)
+    ℓ = 0
+    n_state = 3
+    m_k = @MMatrix zeros(n_state, 1)
+    P_k = MMatrix{3,3}(P∞)
+    P_kbar = @MMatrix zeros(n_state, n_state)
+    K_k = @MMatrix zeros(n_state, 1)
+    for k in 1:n
+        # prediction step
+        m_kbar = A_k * m_k  # state prediction
+        P_kbar .= A_k * P_k * A_k' + Σ_k  # covariance of state prediction, all of the allocations are here
+
+        # update step
+        v_k = y[k] - (H_k * m_kbar)[1]  # difference btw meas and pred, scalar
+        S_k = only(H_k * P_kbar * H_k') + σ²_meas  # P_kbar[1,1] * σ²_kernel + σ²_meas, scalar
+        K_k .= P_kbar * H_k' / S_k  # 3x1
+        # m_k .= m_kbar + SVector{3}(K_k * v_k)
+        m_k = m_kbar + (K_k * v_k)
+        P_k .= P_kbar - K_k * S_k * K_k'
+        ℓ -= log(S_k) + v_k^2/S_k  # 2*ℓ without normalization
+    end
+    return (ℓ - n*log(2π))/2
+end
+
+x_test = 8.78535917650598:6.616545829861497e-7:8.798522794434488
+fx = SOAP_gp(x_test)
+y = rand(fx)
+
+@assert isapprox(TGP.logpdf(fx, y), SOAP_gp_ℓ(y, step(x_test)))
+@assert isapprox(TGP.logpdf(fx, y), SOAP_gp_ℓ_nabla(y, step(x_test)))
