becfae2039b40c2dfaedcd8ce564d64ba9b2cb4c
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2022-01-19 13:07:03 -0500

    adding neid's reduce.jl

--- NEID/reduce.jl
+++ NEID/reduce.jl
@@ -1,6 +1,6 @@
 ## Importing packages
 using Pkg
-Pkg.activate("EXPRES")
+Pkg.activate("NEID")
 Pkg.instantiate()
 
 using JLD2
@@ -11,19 +11,14 @@ using CSV, DataFrames
 
 SSOF_path = dirname(dirname(pathof(SSOF)))
 include(SSOF_path * "/src/_plot_functions.jl")
-stars = ["10700", "26965", "34411"]
+stars = ["10700"]
 orders2inds(selected_orders::AbstractVector) = [searchsortedfirst(orders, order) for order in selected_orders]
 # prep_str = "noreg_"
 prep_str = ""
 
 # for star_ind in 1:2
-star_ind = SSOF.parse_args(1, Int, 2)
-only_excalibur = SSOF.parse_args(2, Bool, true)
-if only_excalibur
-    orders_list = [42:77, 40:77, 38:77]
-else
-    orders_list = [1:85, 1:85, 1:85]
-end
+star_ind = SSOF.parse_args(1, Int, 1)
+orders_list = [4:122]
 star = stars[star_ind]
 orders = orders_list[star_ind]
 
@@ -51,29 +46,7 @@ my_scatter!(plt, x[n_robust], n_comps_bic[n_robust, 2]; alpha=α/2, color=plt_co
 plot!(plt, x, n_comps_bic[:, 1]; label = "", alpha=α/2, color=plt_colors[11], ls=:dot)
 plot!(plt, x, n_comps_bic[:, 2]; label = "", alpha=α/2, color=plt_colors[12], ls=:dot)
 
-png(plt, "expres_$(prep_str)md_$star.png")
-
-# ## Comparing to CCF RVs
-#
-# @load "EXPRES\\alex_stuff\\HD$(star)q0f0n1w1e=false_order_results.jld2" rvs_ccf_orders good_orders order_weights
-# good_orders_mask = [i in orders for i in 12:83] .& good_orders
-# good_orders_2 = (12:83)[good_orders_mask]
-# bad_orders_2 = (12:83)[[i in orders for i in 12:83] .& .!(good_orders)]
-# plt = my_scatter(12:83,order_weights; label="", title="$star Order Weights", xlabel="Orders", c=[i ? :green : :red for i in good_orders])
-# png(plt, "order_weights")
-#
-# using Plots.PlotMeasures
-#
-# myplt(x, y, z) = heatmap(x, y, z; size=(600,400), right_margin=20px, ylabel="orders", xlabel="obs", title="HD"*star)
-# # plt = myplt(1:size(rvs_ccf_orders,2), 12:83, rvs_ccf_orders)
-# plt = myplt(1:size(rvs_ccf_orders,2), (12:83)[good_orders], rvs_ccf_orders[good_orders, :] .- median(rvs_ccf_orders[good_orders, :]; dims=2))
-# png(plt, "test1")
-# ccf_rvs = rvs_ccf_orders[good_orders_mask, :]
-# plt = myplt(1:size(ccf_rvs,2), (12:83)[good_orders_mask], ccf_rvs)
-# png(plt, "test2")
-# ccf_rvs .-= median(ccf_rvs; dims=2)
-# heatmap(ccf_rvs)
-
+png(plt, "neid_$(prep_str)md_$star.png")
 
 ## RV reduction
 
@@ -91,22 +64,22 @@ annot = text.(orders[rvs_std .< σ_floor], :center, :black, 3)
 plt = my_scatter(orders[rvs_std .< σ_floor], rvs_std[rvs_std .< σ_floor]; legend=:topleft, label="", title="$star RV std", xlabel="Order", ylabel="m/s", size=(_plt_size[1]*0.5,_plt_size[2]*0.75), series_annotations=annot, ylim=[0,σ_floor])
 annot = text.(orders[rvs_std .> σ_floor], :center, :black, 3)
 my_scatter!(plt, orders[rvs_std .> σ_floor], ones(sum(rvs_std .> σ_floor)) .* σ_floor; label="", series_annotations=annot, markershape=:utriangle, c=plt_colors[1])
-png(plt, "expres_" * prep_str * star * "_order_rv_std")
+png(plt, "neid_" * prep_str * star * "_order_rv_std")
 
 annot = text.(orders[med_rvs_σ .< σ_floor], :center, :black, 3)
 plt = my_scatter(orders[med_rvs_σ .< σ_floor], med_rvs_σ[med_rvs_σ .< σ_floor]; legend=:topleft, label="", title="$star Median σ", xlabel="Order", ylabel="m/s", size=(_plt_size[1]*0.5,_plt_size[2]*0.75), series_annotations=annot, ylim=[0,σ_floor])
 annot = text.(orders[med_rvs_σ .> σ_floor], :center, :black, 3)
 my_scatter!(plt, orders[med_rvs_σ .> σ_floor], ones(sum(med_rvs_σ .> σ_floor)) .* σ_floor; label="", series_annotations=annot, markershape=:utriangle, c=plt_colors[1])
-png(plt, "expres_" * prep_str * star * "_order_rv_σ")
+png(plt, "neid_" * prep_str * star * "_order_rv_σ")
 
 annot = text.(orders, :center, :black, 3)
 plt = my_scatter(orders, std(rvs; dims=2) ./ med_rvs_σ; legend=:topleft, label="", title="$star (RV std) / (Median σ)", xlabel="Order", size=(_plt_size[1]*0.5,_plt_size[2]*0.75), series_annotations=annot)
-png(plt, "expres_" * prep_str * star * "_order_rv_ratio")
+png(plt, "neid_" * prep_str * star * "_order_rv_ratio")
 
 χ² = vec(sum((rvs .- mean(rvs; dims=2)) .^ 2 ./ (rvs_σ .^ 2); dims=2))
 annot = text.(orders[sortperm(χ²)], :center, :black, 4)
 plt = my_scatter(1:length(χ²), sort(χ²); label="χ²", series_annotations=annot, legend=:topleft, title=prep_str * star * "_χ²") #, yaxis=:log)
-png(plt, "expres_" * prep_str * star * "_χ²")
+png(plt, "neid_" * prep_str * star * "_χ²")
 
 χ²_orders = sortperm(χ²)[1:end-5]
 χ²_orders = [orders[χ²_order] for χ²_order in χ²_orders]
@@ -117,44 +90,13 @@ rvs_red .-= median(rvs_red)
 rvs_σ_red = collect(Iterators.flatten(1 ./ sqrt.(sum(1 ./ (rvs_σ[inds, :] .^ 2); dims=1)')))
 rvs_σ2_red = rvs_σ_red .^ 2
 
-expres_output = CSV.read(SSOF_path * "/EXPRES/" * star * "_activity.csv", DataFrame)
-eo_rv = expres_output."CBC RV [m/s]"
-eo_rv .-= median(eo_rv)
-eo_rv_σ = expres_output."CBC RV Err. [m/s]"
-eo_time = expres_output."Time [MJD]"
+@load SSOF_path * "/NEID/" * star * "_neid_pipeline.jld2" neid_time neid_rv neid_rv_σ
+neid_rv .-= median(neid_rv)
 
 # Compare RV differences to actual RVs from activity
-plt = plot_model_rvs_new(times_nu, rvs_red, rvs_σ_red, eo_time, eo_rv, eo_rv_σ; markerstrokewidth=1, title="HD$star (median σ: $(round(median(rvs_σ_red), digits=3)))")
-png(plt, "expres_" * prep_str * star * "_model_rvs.png")
+plt = plot_model_rvs_new(times_nu, rvs_red, rvs_σ_red, neid_time, neid_rv, neid_rv_σ; markerstrokewidth=1, title="HD$star (median σ: $(round(median(rvs_σ_red), digits=3)))")
+png(plt, "neid_" * prep_str * star * "_model_rvs.png")
 # end
-
-## low component testing plot
-using StatsBase
-use_mad = false
-use_mad ? hmm = mad(lc_rvs; dims=4) : hmm = std(lc_rvs; dims=4)
-hmm[hmm .== 0] .= Inf
-hmm2 = median(lc_rvs_σ; dims=4)
-hmm2[hmm .== Inf] .= Inf
-
-plt = my_plot(orders, std(rvs; dims=2); ylim=[0,10], label="AIC-chosen", legend=:top, xlabel="Order", ylabel="m/s", title="mean RV std per order (HD$star)", ms=3, markerstrokewidth=0, markershape=:circle)
-for i in 1:3
-    for j in 1:3
-        if (j==1 && (i==1 || i==3)) || (i==1 && j==3)
-            plot!(plt, orders, hmm[:, i, j, 1]; label="$(i-1) tel - $(j-1) star", alpha=0.4, ms=2, markerstrokewidth=0, markershape=:circle)
-        else
-            scatter!(plt, orders, hmm[:, i, j, 1]; label="$(i-1) tel - $(j-1) star", alpha=1, ms=2, markerstrokewidth=0)
-        end
-    end
-end
-for i in 1:3
-    for j in 1:3
-        val = 3 * (i - 1) + j
-        scatter!(plt, orders, hmm2[:, i, j, 1]; label="", alpha=0.4, ms=2, markerstrokewidth=0, markershape=:x, c=plt_colors[val])
-    end
-end
-scatter!(plt, orders, median(rvs_σ; dims=2); label="median order σ", alpha=0.4, ms=3, markerstrokewidth=0, markershape=:x, c=plt_colors[1])
-use_mad ? png(plt, "expres_" * prep_str * star * "_lc_mad") : png(plt, "expres_" * prep_str * star * "_lc")
-
 
 ## regularization by order
 
@@ -173,7 +115,7 @@ for i in eachindex(keys)
     # hline!(plt, [SSOF.default_reg_star[keys[i]]], c=plt_colors[i+length(keys)], label="")
 end
 display(plt)
-png(plt, "expres_" * prep_str * star * "_reg_tel")
+png(plt, "neid_" * prep_str * star * "_reg_tel")
 
 plt = _my_plot(;xlabel="Order", ylabel="Regularization", title="Regularizations per order (HD$star)", yaxis=:log)
 # for i in eachindex(keys)
@@ -187,4 +129,4 @@ for i in eachindex(keys)
     hline!(plt, [SSOF.default_reg_star[keys[i]]], c=plt_colors[i], label="")
 end
 display(plt)
-png(plt, "expres_" * prep_str * star * "_reg_star")
+png(plt, "neid_" * prep_str * star * "_reg_star")
