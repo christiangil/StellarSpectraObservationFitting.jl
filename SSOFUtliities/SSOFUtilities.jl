2f19acac510f971f382b0ceef1e8fa3834e695d3
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2022-04-21 12:01:45 -0400

    adding some gathering to SSOFU

--- SSOFUtliities/SSOFUtilities.jl
+++ SSOFUtliities/SSOFUtilities.jl
@@ -3,5 +3,6 @@ module SSOFUtilities
 include("init.jl")
 include("plots.jl")
 include("analysis.jl")
+include("gather.jl")
 
 end # module
---
+++ SSOFUtliities/gather.jl
@@ -0,0 +1,61 @@
+## Some helpful gathering functions
+import StellarSpectraObservationFitting as SSOF
+using JLD2
+
+function safe_retrieve(retrieve_f, args...; kwargs...)
+    try
+        return retrieve_f(args...; kwargs...)
+    catch err
+        if isa(err, SystemError)
+            println("order is missing")
+        else
+            rethrow()
+        end
+    end
+end
+
+function _retrieve_rvs(fn::String; return_extra::Bool=false)
+    @load fn model rv_errors
+    rvs_notel_opt = SSOF.rvs(model)
+    if return_extra
+        SSOF.is_time_variable(model.tel) ? n_comp_tel = size(model.tel.lm.M, 2) : n_comp_tel = 0
+        SSOF.is_time_variable(model.star) ? n_comp_star = size(model.star.lm.M, 2) : n_comp_star = 0
+        return rvs_notel_opt, rv_errors, [n_comp_tel, n_comp_star], model.reg_tel, model.reg_star
+    end
+    return rvs_notel_opt, rv_errors
+end
+retrieve_rvs(args...; kwargs...) = safe_retrieve(_retrieve_rvs, args...; kwargs...)
+
+function _retrieve_md(fn::String)
+    @load fn comp_ls ℓ aics bics ks test_n_comp_tel test_n_comp_star comp_stds comp_intra_stds
+    return comp_ls, ℓ, aics, bics, ks, test_n_comp_tel, test_n_comp_star, comp_stds, comp_intra_stds
+end
+retrieve_md(args...; kwargs...) = safe_retrieve(_retrieve_md, args...; kwargs...)
+
+function retrieve_all_rvs(n_obs::Int, fns::Vector{String})
+    n_ord = length(fns)
+    rvs = zeros(n_ord,  n_obs)
+    rvs_σ = Inf .* ones(n_ord, n_obs)
+    for i in 1:n_ord
+        try
+            rvs[i, :], rvs_σ[i, :] = SSOFU._retrieve_rvs(fns[i])
+        catch err
+            if isa(err, SystemError)
+                println("order $(orders[i]) is missing")
+            else
+                rethrow()
+            end
+        end
+    end
+    return rvs, rvs_σ
+end
+
+function retrieve_all_rvs(data_fns::Vector{String}, fns::Vector{Vector{String}}, save_fns::Vector{String})
+    @assert length(fns) == length(save_fns) == length(data_fns)
+    for i in 1:length(save_fns)
+        @load data_fns[i] n_obs times_nu airmasses
+        n_ord = length(fns[i])
+        rvs, rvs_σ = retrieve_all_rvs(n_obs, fns[i])
+        @save save_fns[i] rvs rvs_σ n_obs times_nu airmasses n_ord
+    end
+end
