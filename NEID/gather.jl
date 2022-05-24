0a781cbd171818a624a7656e787347df2babf95f
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2022-05-24 12:53:41 -0400

    add wobble to gather.jl

--- NEID/gather.jl
+++ NEID/gather.jl
@@ -12,16 +12,23 @@ SSOFU = SSOFUtilities
 ## Setting up necessary variables
 
 input_ind = SSOF.parse_args(1, Int, 0)
+dpca = SSOF.parse_args(2, Bool, true)
 
 stars = ["10700", "26965", "9407", "185144", "2021/12/19", "2021/12/20", "2021/12/23"]
 input_ind == 0 ? star_inds = (1:length(stars)) : star_inds = input_ind:input_ind
 orders_list = repeat([4:122], length(stars))
 include("data_locs.jl")  # defines neid_data_path and neid_save_path
 # prep_str = "noreg_"
-prep_str = ""
+if dpca
+    prep_str_load = ""
+    prep_str_save = ""
+else
+    prep_str_load = "wobble/"
+    prep_str_save = "wobble_"
+end
 
 SSOFU.retrieve_all_rvs(
     [neid_save_path*star*"/60/data.jld2" for star in stars[star_inds]],
-    [[neid_save_path*stars[i]*"/$order/$(prep_str)results.jld2" for order in orders_list[i]] for i in star_inds],
-    [replace("neid_$(prep_str)$(star)_rvs.jld2", "/" => "_") for star in stars[star_inds]]
+    [[neid_save_path*stars[i]*"/$order/$(prep_str_load)results.jld2" for order in orders_list[i]] for i in star_inds],
+    [replace("jld2/neid_$(prep_str_save)$(star)_rvs.jld2", "/" => "_") for star in stars[star_inds]]
     )
