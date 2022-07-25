884d7dcce2be9d98d5ded8f5420c87604be1fef3
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2022-07-25 11:13:03 -0400

    new reduce plots

--- NEID/reduce.jl
+++ NEID/reduce.jl
@@ -18,12 +18,13 @@ stars = ["10700", "26965", "9407", "185144", "22049", "2021/12/19", "2021/12/20"
 orders_list = repeat([4:122], length(stars))
 include("data_locs.jl")  # defines neid_data_path and neid_save_path
 star_ind = SSOF.parse_args(1, Int, 2)
-dpca = SSOF.parse_args(2, Bool, true)
+dpca = SSOF.parse_args(2, Bool, false)
 if dpca
     prep_str = "dpca_"
 else
     prep_str = "wobble_"
 end
+save_fn = "figs/neid_" * prep_str * star * "_"
 
 # for star_ind in [1,2,5]
 #     for prep_str in ["wobble_", ""]
@@ -46,7 +47,7 @@ rvs_std = vec(std(rvs; dims=2))
 χ²_thres = 4.5e3 / 37 * size(rvs, 2)
 χ²_orders = [i for i in χ²_sortperm if χ²[i] < χ²_thres]
 χ²_orders = [orders[χ²_order] for χ²_order in χ²_orders]
-inds = orders2inds([orders[i] for i in eachindex(orders) if ((rvs_std[i] < σ_floor) && (med_rvs_σ[i] < σ_floor) && (orders[i] in χ²_orders))])
+inds = orders2inds([orders[i] for i in eachindex(orders) if ((rvs_std[i] < σ_floor) && (med_rvs_σ[i] < σ_floor/5) && (orders[i] in χ²_orders))])
 println("starting with $(length(orders)) orders")
 println("$(length(orders) - length(χ²_sortperm)) orders ignored for unfinished analyses or otherwise weird")
 println("$(length(χ²_sortperm) - length(χ²_orders)) worst orders (in χ²-sense) ignored")
@@ -70,30 +71,56 @@ plt = SSOFU._plot()
 scatter!(plt, orders[rvs_std .< σ_floor], rvs_std[rvs_std .< σ_floor]; legend=:topleft, label="", title="$star RV std", xlabel="Order", ylabel="m/s", size=(SSOFU._plt_size[1]*0.5,SSOFU._plt_size[2]*0.75), ylim=[0,σ_floor], series_annotations=annot, markerstrokewidth=0.5)
 annot = text.(orders[rvs_std .> σ_floor], :center, :black, 3)
 scatter!(plt, orders[rvs_std .> σ_floor], ones(sum(rvs_std .> σ_floor)) .* σ_floor; label="", series_annotations=annot, markershape=:utriangle, c=SSOFU.plt_colors[1], markerstrokewidth=0.5)
-png(plt, "figs/neid_" * prep_str * star * "_order_rv_std")
+png(plt, save_fn * "order_rv_std")
 
 annot = text.(orders[med_rvs_σ .< σ_floor], :center, :black, 3)
 plt = SSOFU._plot()
 scatter!(plt, orders[med_rvs_σ .< σ_floor], med_rvs_σ[med_rvs_σ .< σ_floor]; legend=:topleft, label="", title="$star Median σ", xlabel="Order", ylabel="m/s", size=(SSOFU._plt_size[1]*0.5,SSOFU._plt_size[2]*0.75), series_annotations=annot, ylim=[0,σ_floor], markerstrokewidth=0.5)
 annot = text.(orders[med_rvs_σ .> σ_floor], :center, :black, 3)
 scatter!(plt, orders[med_rvs_σ .> σ_floor], ones(sum(med_rvs_σ .> σ_floor)) .* σ_floor; label="", series_annotations=annot, markershape=:utriangle, c=SSOFU.plt_colors[1], markerstrokewidth=0.5)
-png(plt, "figs/neid_" * prep_str * star * "_order_rv_σ")
+png(plt, save_fn * "order_rv_σ")
 
 annot = text.(orders, :center, :black, 3)
 plt = SSOFU._plot()
 scatter!(plt, orders, std(rvs; dims=2) ./ med_rvs_σ; legend=:topleft, label="", title="$star (RV std) / (Median σ)", xlabel="Order", size=(SSOFU._plt_size[1]*0.5,SSOFU._plt_size[2]*0.75), series_annotations=annot, markerstrokewidth=0.5)
-png(plt, "figs/neid_" * prep_str * star * "_order_rv_ratio")
+png(plt, save_fn * "order_rv_ratio")
 
 annot = text.(orders[χ²_sortperm], :center, :black, 4)
-plt = SSOFU._plot()
-scatter!(plt, χ²[χ²_sortperm]; label="", ylabel="χ²", series_annotations=annot, title = star * "_χ²", size=(SSOFU._plt_size[1]*(length(orders) / 80),SSOFU._plt_size[2]), markerstrokewidth=0, yaxis=:log10)
+plt = SSOFU._plot(; ylabel="χ²", title = star * "_χ²", size=(SSOFU._plt_size[1]*(length(orders) / 80),SSOFU._plt_size[2]), yaxis=:log10)
+scatter!(plt, χ²[χ²_sortperm]; label="", series_annotations=annot, markerstrokewidth=0)
 hline!(plt, [χ²_thres]; label="", lw=1, color=SSOFU.plt_colors[1])
-png(plt, "figs/neid_" * prep_str * star * "_χ²")
+png(plt, save_fn * "χ²")
 
 # Compare RV differences to actual RVs from activity
 plt = SSOFU.plot_model_rvs(times_nu[mask], rvs_red[mask], rvs_σ_red[mask], neid_time[mask], neid_rv[mask], neid_rv_σ[mask]; markerstrokewidth=0.5, title="HD $star (median σ: $(round(median(rvs_σ_red), digits=3)))")
-png(plt, "figs/neid_" * prep_str * star * "_model_rvs.png")
+png(plt, save_fn * "model_rvs.png")
 plt = SSOFU.plot_model_rvs(times_nu[mask], rvs_red[mask] .- lin.(times_nu[mask]), rvs_σ_red[mask], neid_time[mask], neid_rv[mask], neid_rv_σ[mask]; markerstrokewidth=0.5, title="HD $star (median σ: $(round(median(rvs_σ_red), digits=3)))")
-png(plt, "figs/neid_" * prep_str * star * "_model_rvs_detrend.png")
+png(plt, save_fn * "model_rvs_detrend.png")
 #     end
 # end
+
+# RV differnces as a function of order (annot somehow breaks everything?)
+x = rvs[inds, :] .- rvs_red'
+x_σ = vec(median(rvs_σ[inds, :]; dims=2))
+# annot = text.(orders[inds], :center, :black, 3)
+plt = SSOFU._plot(; xlabel="Order", ylabel="std(RV_order - RV_bulk) (m/s)", title = star, size=(SSOFU._plt_size[1]*(length(orders) / 80),SSOFU._plt_size[2]))
+scatter!(plt, orders[inds], vec(std(x; dims=2)); yerror=x_σ, label="", markerstrokewidth=0.5)#, series_annotations=annot)
+png(plt, save_fn * "order_dif")
+
+# order rv correlation heatmap
+n_ord = size(rvs, 1)
+cors = Array{Float64}(undef, n_ord, n_ord)
+for i in 1:n_ord
+    for j in i+1:n_ord
+        cors[i, j] = cor(view(rvs, i, :), view(rvs, j, :))
+    end
+end
+cors[diagind(cors)] .= 1
+cors = Symmetric(cors)
+heatmap(cors)
+
+plt = SSOFU._plot(;ylabel="order")
+scatter!(orders, [cor(view(rvs, i, :), rvs_red) for i in 1:n_ord]; yerror=x_σ./40, ylims=(-0.5, 1.1), markerstrokewidth=0.5, label="Unused Orders")
+scatter!(orders[inds], [cor(view(rvs, i, :), rvs_red) for i in 1:n_ord][inds]; label="Used Orders", markerstrokewidth=0.5)
+
+inds
