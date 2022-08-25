f8516d64f4be44e10e09e2b64c1339bf4eb2dd71
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2022-08-25 18:10:40 -0400

    more multithreading support that only works for the bootstrapping and not the curvature for some reason

--- SSOFUtilities/analysis.jl
+++ SSOFUtilities/analysis.jl
@@ -220,14 +220,14 @@ function estimate_σ_curvature(mws::SSOF.ModelWorkspace; recalc::Bool=false, sav
 
 		if time_var_tel
 			ℓ_tel(x) = SSOF._loss(mws.o, model, mws.d; tel=vec(model.tel.lm)) + SSOF.model_s_prior(model.tel.lm.s, model.reg_tel)
-			tel_s_σ = reshape(estimate_σ_curvature_helper(model.tel.lm.s, ℓ_tel; param_str="tel_s", kwargs...), size(model.tel.lm.s))
+			tel_s_σ = estimate_σ_curvature_helper(model.tel.lm.s, ℓ_tel; param_str="tel_s", kwargs...)
 		else
 			tel_s_σ = nothing
 		end
 
 		if time_var_star
 			ℓ_star(x) = SSOF._loss(mws.o, model, mws.d; star=vec(model.star.lm)) + SSOF.model_s_prior(model.star.lm.s, model.reg_star)
-			star_s_σ = reshape(estimate_σ_curvature_helper(model.star.lm.s, ℓ_star; param_str="star_s", kwargs...), size(model.star.lm.s))
+			star_s_σ = estimate_σ_curvature_helper(model.star.lm.s, ℓ_star; param_str="star_s", kwargs...)
 		else
 			star_s_σ = nothing
 		end
@@ -243,51 +243,77 @@ function estimate_σ_curvature(mws::SSOF.ModelWorkspace; recalc::Bool=false, sav
 end
 
 
-using Nabla
-function estimate_σ_curvature_helper(x::AbstractVecOrMat, ℓ::Function; n::Int=7, param_str::String="", print_every::Int=10, use_gradient::Bool=false, print_stuff::Bool=false, show_plots::Bool=false)
-	x_test = Array{Float64}(undef, n)
+function estimate_σ_curvature_helper(x::AbstractVecOrMat, ℓ::Function; n::Int=7, use_gradient::Bool=false, multithread::Bool=false, kwargs...)
 	σs = Array{Float64}(undef, length(x))
-	ℓs = Array{Float64}(undef, n)
+	if !multithread
+		x_test = Array{Float64}(undef, n)
+		ℓs = Array{Float64}(undef, n)
+	end
 	if use_gradient; g = ∇(ℓ) end
 	_std = std(x)
-	for i in 1:length(x)
-		hold = x[i]
-		# x_test[:] = x[i] .+ LinRange(-_std/1e3, _std/1e3, n)
-		x_test[:] = x[i] .+ LinRange(-_std, _std, n)
-		for j in 1:n
-			x[i] = x_test[j]
-			if use_gradient
-				ℓs[j] = only(g(x))[i]
-			else
-				ℓs[j] = ℓ(x)
+	if multithread
+		@threads for i in 1:length(x)
+			# _x = deepcopy(x)
+			_x = copy(x)
+			# x_test = _x[i] .+ LinRange(-_std/1e3, _std/1e3, n)
+			x_test = _x[i] .+ LinRange(-_std, _std, n)
+			# _ℓs = ones(n)
+			_ℓs = Array{Float64}(undef, n)
+			for j in 1:n
+				_x[i] = x_test[j]
+				if use_gradient
+					_ℓs[j] = only(g(_x))[i]
+				else
+					_ℓs[j] = ℓ(_x)
+				end
 			end
+			println("$i: ", _ℓs .- _ℓs[Int(round(n//2))])
+			estimate_σ_curvature_helper_finalizer!(σs, _ℓs, x_test, i; use_gradient=use_gradient, kwargs...)
 		end
-		x[i] = hold
-
-		if use_gradient
-			poly_f = SSOF.ordinary_lst_sq_f(ℓs, 1; x=_x_test)
-			σs[i] = sqrt(1 / poly_f.w[2])
-			max_dif = maximum(abs.((poly_f.(x_test)./ℓs) .- 1))
-			if print_stuff; println("∇_$i: $(poly_f.w[1] + poly_f.w[2] * x[i])") end
-		else
-			poly_f = SSOF.ordinary_lst_sq_f(ℓs, 2; x=x_test)
-			σs[i] = sqrt(1 / (2 * poly_f.w[3]))
-			max_dif = maximum(abs.((poly_f.(x_test)./ℓs) .- 1))
-			if print_stuff; println("∇_$i: $(poly_f.w[2] + 2 * poly_f.w[3] * x[i])") end
+	else
+		for i in 1:length(x)
+			hold = x[i]
+			# x_test[:] = x[i] .+ LinRange(-_std/1e3, _std/1e3, n)
+			x_test[:] = x[i] .+ LinRange(-_std, _std, n)
+			for j in 1:n
+				x[i] = x_test[j]
+				if use_gradient
+					ℓs[j] = only(g(x))[i]
+				else
+					ℓs[j] = ℓ(x)
+				end
+			end
+			x[i] = hold
+			println("$i: ", ℓs .- ℓs[Int(round(n//2))])
+			estimate_σ_curvature_helper_finalizer!(σs, ℓs, x_test, i; use_gradient=use_gradient, kwargs...)
 		end
-		if show_plots
-			plt = scatter(x_test, ℓs; label="ℓ")
-			plot!(x_test, poly_f.(x_test); label="polynomial fit")
-			display(plt)
-		end
-		if max_dif > 1e-2; @warn param_str * "_σ[$i] misfit at $(round(100*max_dif; digits=2))% level" end
-		if i%print_every==0; println("done with $i/$(length(x)) " * param_str * "_σ estimates") end
-		if i%length(x)==0; println("done with all " * param_str * "_σ estimates") end
 	end
-	return σs
+	return reshape(σs, size(x))
 end
 
-function estimate_σ_bootstrap_helper(shaper::AbstractArray, holder::AbstractArray, reducer::Function)
+function estimate_σ_curvature_helper_finalizer!(σs::AbstractVecOrMat, _ℓs::Vector, x_test::AbstractVector, i::Int; use_gradient::Bool=false, param_str::String="", print_every::Int=10, print_stuff::Bool=false, show_plots::Bool=false, )
+	if use_gradient
+		poly_f = SSOF.ordinary_lst_sq_f(_ℓs, 1; x=x_test)
+		σs[i] = sqrt(1 / poly_f.w[2])
+		max_dif = maximum(abs.((poly_f.(x_test)./_ℓs) .- 1))
+		if print_stuff; println("∇_$i: $(poly_f.w[1] + poly_f.w[2] * x[i])") end
+	else
+		poly_f = SSOF.ordinary_lst_sq_f(_ℓs, 2; x=x_test)
+		σs[i] = sqrt(1 / (2 * poly_f.w[3]))
+		max_dif = maximum(abs.((poly_f.(x_test)./_ℓs) .- 1))
+		if print_stuff; println("∇_$i: $(poly_f.w[2] + 2 * poly_f.w[3] * x[i])") end
+	end
+	if show_plots
+		plt = scatter(x_test, _ℓs; label="ℓ")
+		plot!(x_test, poly_f.(x_test); label="polynomial fit")
+		display(plt)
+	end
+	if max_dif > 1e-2; @warn param_str * "_σ[$i] misfit at $(round(100*max_dif; digits=2))% level" end
+	if i%print_every==0; println("done with $i/$(length(σs)) " * param_str * "_σ estimates") end
+	# println("done with $i/$(length(σs)) " * param_str * "_σ estimates")
+end
+
+function estimate_σ_bootstrap_reducer(shaper::AbstractArray, holder::AbstractArray, reducer::Function)
 	result = Array{Float64}(undef, size(shaper, 1), size(shaper, 2))
 	for i in 1:size(shaper, 1)
 		result[i, :] .= vec(reducer(view(holder, :, i, :); dims=1))
@@ -295,64 +321,72 @@ function estimate_σ_bootstrap_helper(shaper::AbstractArray, holder::AbstractArr
 	return result
 end
 
-function estimate_σ_bootstrap(mws::SSOF.ModelWorkspace; recalc::Bool=false, save_fn::String="", n::Int=50, return_holders::Bool=false, recalc_mean::Bool=false)
+function estimate_σ_bootstrap_helper!(rv_holder::AbstractMatrix, tel_holder, star_holder, i::Int, mws::SSOF.ModelWorkspace, data_noise::AbstractMatrix, n::Int)
+	time_var_tel = SSOF.is_time_variable(mws.om.tel)
+	time_var_star = SSOF.is_time_variable(mws.om.star)
+	_mws = typeof(mws)(copy(mws.om), copy(mws.d))
+	_mws.d.flux .= mws.d.flux .+ (data_noise .* randn(size(mws.d.var)))
+	improve_model!(_mws, iter=50, print_stuff=false)
+	rv_holder[i, :] = SSOF.rvs(_mws.om)
+	if time_var_tel
+		tel_holder[i, :, :] .= _mws.om.tel.lm.s
+	end
+	if time_var_star
+		star_holder[i, :, :] .= _mws.om.star.lm.s
+	end
+	if i%10==0; println("done with $i/$n bootstraps") end
+end
+
+function estimate_σ_bootstrap(mws::SSOF.ModelWorkspace; recalc::Bool=false, save_fn::String="", n::Int=50, return_holders::Bool=false, recalc_mean::Bool=false, multithread::Bool=nthreads() > 4)
 	save = save_fn!=""
-	model = mws.om
-	if recalc || !model.metadata[:todo][:err_estimated] # 25 mins
+	if recalc || !mws.om.metadata[:todo][:err_estimated] # 25 mins
 	    mws.d.var[mws.d.var.==Inf] .= 0
 	    data_noise = sqrt.(mws.d.var)
 	    mws.d.var[mws.d.var.==0] .= Inf
 
 	    typeof(mws.om) <: SSOF.OrderModelWobble ?
-		 	rv_holder = Array{Float64}(undef, n, length(model.rv)) :
-			rv_holder = Array{Float64}(undef, n, length(model.rv.lm.s))
+		 	rv_holder = Array{Float64}(undef, n, length(mws.om.rv)) :
+			rv_holder = Array{Float64}(undef, n, length(mws.om.rv.lm.s))
 
 		time_var_tel = SSOF.is_time_variable(mws.om.tel)
 		time_var_star = SSOF.is_time_variable(mws.om.star)
-		if time_var_tel
-			tel_holder = Array{Float64}(undef, n, size(mws.om.tel.lm.s, 1), size(mws.om.tel.lm.s, 2))
-		end
-		if time_var_star
-			star_holder = Array{Float64}(undef, n, size(mws.om.star.lm.s, 1), size(mws.om.star.lm.s, 2))
-		end
-
-	    for i in 1:n
-			_mws = typeof(mws)(copy(model), copy(mws.d))
-	        _mws.d.flux .= mws.d.flux .+ (data_noise .* randn(size(mws.d.var)))
-			improve_model!(_mws, iter=50, print_stuff=false)
-	        rv_holder[i, :] = SSOF.rvs(_mws.om)
-			if time_var_tel
-				tel_holder[i, :, :] .= _mws.om.tel.lm.s
+		time_var_tel ?
+			tel_holder = Array{Float64}(undef, n, size(mws.om.tel.lm.s, 1), size(mws.om.tel.lm.s, 2)) :
+			tel_holder = nothing
+		time_var_star ?
+			star_holder = Array{Float64}(undef, n, size(mws.om.star.lm.s, 1), size(mws.om.star.lm.s, 2)) :
+			star_holder = nothing
+		if multithread
+			@threads for i in 1:n
+				estimate_σ_bootstrap_helper!(rv_holder, tel_holder, star_holder, i, mws, data_noise, n)
 			end
-			if time_var_star
-				star_holder[i, :, :] .= _mws.om.star.lm.s
+		else
+			for i in 1:n
+				estimate_σ_bootstrap_helper!(rv_holder, tel_holder, star_holder, i, mws, data_noise, n)
 			end
-			if i%10==0; println("done with $i/$n bootstraps") end
 	    end
-		recalc_mean ? rvs = vec(mean(rv_holder; dims=1)) : rvs = SSOF.rvs(model)
+		recalc_mean ? rvs = vec(mean(rv_holder; dims=1)) : rvs = SSOF.rvs(mws.om)
 	    rvs_σ = vec(std(rv_holder; dims=1))
 
 		if time_var_tel
 			recalc_mean ?
-				tel_s = estimate_σ_bootstrap_helper(mws.om.tel.lm.s, tel_holder, mean) :
+				tel_s = estimate_σ_bootstrap_reducer(mws.om.tel.lm.s, tel_holder, mean) :
 				tel_s = mws.om.tel.lm.s
-			tel_s_σ = estimate_σ_bootstrap_helper(mws.om.tel.lm.s, tel_holder, std)
+			tel_s_σ = estimate_σ_bootstrap_reducer(mws.om.tel.lm.s, tel_holder, std)
 		else
 			tel_s = nothing
 			tel_s_σ = nothing
-			tel_holder = nothing
 		end
 		if time_var_star
 			recalc_mean ?
-				star_s = estimate_σ_bootstrap_helper(mws.om.star.lm.s, star_holder, mean) :
+				star_s = estimate_σ_bootstrap_reducer(mws.om.star.lm.s, star_holder, mean) :
 				star_s = mws.om.star.lm.s
-			star_s_σ = estimate_σ_bootstrap_helper(mws.om.star.lm.s, star_holder, std)
+			star_s_σ = estimate_σ_bootstrap_reducer(mws.om.star.lm.s, star_holder, std)
 		else
 			star_s = nothing
 			star_s_σ = nothing
-			star_holder = nothing
 		end
-		model.metadata[:todo][:err_estimated] = true
+		mws.om.metadata[:todo][:err_estimated] = true
 	    if save; @save save_fn rvs rvs_σ tel_s tel_s_σ star_s star_s_σ end
 		if return_holders
 			return rvs, rvs_σ, tel_s, tel_s_σ, star_s, star_s_σ, rv_holder, tel_holder, star_holder
