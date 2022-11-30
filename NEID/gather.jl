1e1253dbe408604bd7207d9a55384c9406364602
Chistian Gilbertson <32468452+christiangil@users.noreply.github.com>
2022-11-30 15:29:35 -0500

    better gather.jl

--- NEID/gather.jl
+++ NEID/gather.jl
@@ -12,21 +12,21 @@ SSOFU = SSOFUtilities
 ## Setting up necessary variables
 
 input_ind = SSOF.parse_args(1, Int, 2)
-bootstrap = SSOF.parse_args(2, Bool, true)
-log_lm = SSOF.parse_args(3, Bool, true)
-dpca = SSOF.parse_args(4, Bool, false)
-use_lsf = SSOF.parse_args(5, Bool, true)
-by_eye = SSOF.parse_args(6, Bool, false)
+folder = SSOF.parse_args(2, String, "aic")
+bootstrap = SSOF.parse_args(3, Bool, true)
+log_lm = SSOF.parse_args(4, Bool, true)
+dpca = SSOF.parse_args(5, Bool, false)
+use_lsf = SSOF.parse_args(6, Bool, true)
 
 stars = ["10700", "26965", "22049", "3651", "95735", "2021/12/19", "2021/12/20", "2021/12/23"]
-input_ind == 0 ? star_inds = (eachindex(stars)) : star_inds = input_ind:input_ind
+input_ind == 0 ? star_inds = (1:length(stars)) : star_inds = input_ind:input_ind
 orders_list = repeat([7:118], length(stars))
 include("data_locs.jl")  # defines neid_data_path and neid_save_path
 bootstrap ? appe_str = "_boot" : appe_str = "_curv"
 log_lm ? prep_str = "log_" : prep_str = "lin_"
 dpca ? prep_str *= "dcp_" : prep_str *= "vil_"
 use_lsf ? prep_str *= "lsf/" : prep_str *= "nol/"
-by_eye ? prep_str *= "by_eye/" : prep_str *= "aic/"
+prep_str *= folder * "/"
 
 SSOFU.retrieve(
     ["jld2/neid_" * replace("$(prep_str)$(star)_rvs$(appe_str).jld2", "/" => "_") for star in stars[star_inds]],
